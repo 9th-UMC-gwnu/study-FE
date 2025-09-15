fb2042190f06b6b340d6355ec746de480c8e962e
kim10914 <43512783+kim10914@users.noreply.github.com>
2025-09-16 02:50:02 +0900

    week1/mission : TodoList

--- 김성환/study/src/App.tsx
+++ 김성환/study/src/App.tsx
@@ -1,14 +1,33 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import TodoPage from "./week-no/week-1/mission/mission1"
 
-const AppRouter = ()=>{
-  return(
-    <div>라우터</div>
+const AppRoute = () => {
+  return (
+    <Routes>
+      <Route path="/week1-mission1" element={<TodoPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
+
+const AppNav = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <Link to="/week1-mission1">1week-mission1</Link>
+    </div>
+  );
+};
+
+const HomePage = () => { // 대충 링크 페이지
+  return <AppNav />;
+};
 
 function App() {
 
   return (
-    <div>메인</div>
+    <BrowserRouter>
+        <AppRoute />
+    </BrowserRouter>
   )
 }
 
--- 김성환/study/src/week-no/week-1/mission/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function mission1(){
-    return(
-        <div> 미션</div>
-    )
-}
