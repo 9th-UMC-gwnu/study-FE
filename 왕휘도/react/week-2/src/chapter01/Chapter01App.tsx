07e6a94e52253a5b9cc293e880d2f3da9f227f52
luminance <kingluminance@gmail.com>
2025-09-28 03:41:24 +0900

    fix : 다크모드 디자인 수정

--- 왕휘도/react/week-2/src/chapter01/Chapter01App.tsx
+++ 왕휘도/react/week-2/src/chapter01/Chapter01App.tsx
@@ -27,17 +27,27 @@ export default function Chapter01App() {
         {theme === "LIGHT" ? "라이트 모드" : "다크 모드"}
       </button>
       <div className="flex items-center justify-center h-screen flex-col mx-28">
-        <h1 className="text-5xl font-bold">YONG TODO</h1>
+        <h1
+          className={clsx(
+            "text-5xl font-bold",
+            theme === "DARK" ? "text-white" : "text-black"
+          )}
+        >
+          YONG TODO
+        </h1>
         <div className="flex items-center my-6 w-full justify-between">
           <input
             type="text"
             placeholder="할 일 입력"
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className="rounded-sm p-3 border border-zinc-400 mr-4 flex-2"
+            className={clsx(
+              "rounded-sm p-3 border border-zinc-400 mr-4 flex-2",
+              theme === "DARK" ? "text-white" : "text-black"
+            )}
           />
           <button onClick={handleAdd} className="bg-green-500 p-4 rounded-sm">
-            <h3 className="text-white text-sm">할 일 추가</h3>
+            <h3 className={"text-white text-sm"}>할 일 추가</h3>
           </button>
         </div>
         <ListBox />
--- 왕휘도/react/week-2/src/chapter01/components/ListBox.tsx
+++ 왕휘도/react/week-2/src/chapter01/components/ListBox.tsx
@@ -1,16 +1,33 @@
+import clsx from "clsx";
 import DoneList from "./DoneList";
 import YetList from "./YetList";
+import { useTheme } from "../../chapter02/ThemeProvider";
 
 export default function ListBox() {
+  const { theme } = useTheme();
   return (
     <div className="flex flex-row gap-7 w-full">
       <div className="w-full justify-between flex-1">
-        <h1 className="text-3xl font-bold w-full text-center">할 일</h1>
+        <h1
+          className={clsx(
+            "text-3xl font-bold w-full text-center",
+            theme === "DARK" ? "text-white" : "text-black"
+          )}
+        >
+          할 일
+        </h1>
         <YetList />
       </div>
 
       <div className="w-full justify-between flex-1">
-        <h1 className="text-3xl font-bold text-center">완료</h1>
+        <h1
+          className={clsx(
+            "text-3xl font-bold w-full text-center",
+            theme === "DARK" ? "text-white" : "text-black"
+          )}
+        >
+          완료
+        </h1>
         <DoneList />
       </div>
     </div>
