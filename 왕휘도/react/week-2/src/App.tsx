0cc0838b22887e2372f17ffb03f0e3fba76ab6b5
luminance <kingluminance@gmail.com>
2025-09-27 22:18:40 +0900

    feat : useContext 예제

--- 왕휘도/react/week-2/src/App.tsx
+++ 왕휘도/react/week-2/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { StudyApp } from "./study";
+//import { StudyApp } from "./study";
+import StudyApp2 from "./study2/StudyApp2";
 //index.ts 도입
 
 function App() {
   return (
     <>
-      <StudyApp />
+      <StudyApp2 />
     </>
   );
 }
