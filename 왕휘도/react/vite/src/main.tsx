041c7c1a45d83b42aa49186629ae22bcfc000fc6
luminance <kingluminance@gmail.com>
2025-10-03 16:30:15 +0900

    chore : main BrowserRouter 추가

--- 왕휘도/react/vite/src/main.tsx
+++ 왕휘도/react/vite/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
-import { TodoProvider } from "./chapter01/context/TodoProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TodoProvider>
+    <BrowserRouter>
       <App />
-    </TodoProvider>
+    </BrowserRouter>
   </StrictMode>
 );
